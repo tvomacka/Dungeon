e3fb441d8d6743a2ac071a497a42c2dd9807543c
tvomacka <66327737+tvomacka@users.noreply.github.com>
2022-11-24 20:58:59 +0100

    R!! DialogueState Partial Removed

--- Dungeon/Dialogues/DialogueState.cs
+++ Dungeon/Dialogues/DialogueState.cs
@@ -1,23 +1,22 @@
-﻿namespace Dungeon.Dialogues
-{
-    public partial class Dialogue
-    {
-        public class DialogueState
-        {
-            public int Id { get; set; }
-            public string Text { get; set; }
-            public DialogueOption[] Options { get; set; }
+﻿using static Dungeon.Dialogues.Dialogue;
 
-            public override string ToString()
+namespace Dungeon.Dialogues
+{
+    public class DialogueState
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DialogueOption[] Options { get; set; }
+
+        public override string ToString()
+        {
+            var text = Text;
+            var optionId = 0;
+            foreach (var option in Options)
             {
-                var text = Text;
-                var optionId = 0;
-                foreach (var option in Options)
-                {
-                    text += $"\n\t{optionId++}: {option}";
-                }
-                return text;
+                text += $"\n\t{optionId++}: {option}";
             }
+            return text;
         }
     }
 }
