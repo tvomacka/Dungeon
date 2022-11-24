dcfd568b0de849f3d162b134e00a4fb8f40f5637
tvomacka <66327737+tvomacka@users.noreply.github.com>
2022-11-24 21:01:48 +0100

    r Extracted DialogueCondition

--- Dungeon/Dialogues/Dialogue.cs
+++ Dungeon/Dialogues/Dialogue.cs
@@ -66,28 +66,6 @@ namespace Dungeon.Dialogues
         }
     }
 
-    public class DialogueCondition
-    {
-        public string Subject { get; set; }
-        public string Test { get; set; }
-        public string Target { get; set; }
-
-        public bool IsSatisfied()
-        {
-            if (Subject == "Intelligence" && Test == "GreaterThan")
-                return Game.Instance.Party[0].Intelligence > int.Parse(Target);
-            else if (Subject == "Item" && Test == "InInventory")
-                return Game.Instance.Party[0].Inventory.Contains(int.Parse(Target));
-
-            return false;
-        }
-
-        public override string ToString()
-        {
-            return $"{Subject} {Test} {Target}";
-        }
-    }
-
     public class DialogueAction
     {
         public string ActionType { get; set; }
---
+++ Dungeon/Dialogues/DialogueCondition.cs
@@ -0,0 +1,26 @@
+﻿using Dungeon.GameLogic;
+
+namespace Dungeon.Dialogues
+{
+    public class DialogueCondition
+    {
+        public string Subject { get; set; }
+        public string Test { get; set; }
+        public string Target { get; set; }
+
+        public bool IsSatisfied()
+        {
+            if (Subject == "Intelligence" && Test == "GreaterThan")
+                return Game.Instance.Party[0].Intelligence > int.Parse(Target);
+            else if (Subject == "Item" && Test == "InInventory")
+                return Game.Instance.Party[0].Inventory.Contains(int.Parse(Target));
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{Subject} {Test} {Target}";
+        }
+    }
+}
