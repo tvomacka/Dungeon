5b61786398fde458e6068b918f2ea1dfa2cb39b0
tvomacka <66327737+tvomacka@users.noreply.github.com>
2022-11-24 21:02:25 +0100

    r DialogueAction Extracted

---
+++ Dungeon/Dialogues/DialogueAction.cs
@@ -0,0 +1,22 @@
+﻿using Dungeon.GameLogic;
+
+namespace Dungeon.Dialogues
+{
+    public class DialogueAction
+    {
+        public string ActionType { get; set; }
+        public string[] ActionParameters { get; set; }
+
+        public void Execute()
+        {
+            if (ActionType == "AcceptQuest")
+                Game.Instance.Party.ActiveQuests.Add(int.Parse(ActionParameters[0]));
+            else if (ActionType == "FinishQuest")
+                Game.Instance.Party.ActiveQuests.Remove(int.Parse(ActionParameters[0]));
+            else if (ActionType == "LoseItem")
+                Game.Instance.Party[0].Inventory.Remove(int.Parse(ActionParameters[0]));
+            else if (ActionType == "GainXP")
+                Game.Instance.Party[0].AddXP(int.Parse(ActionParameters[0]));
+        }
+    }
+}
