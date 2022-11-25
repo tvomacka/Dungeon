ca7e27b36b1b6491e946b306cc12e88b258a0a1c
tvomacka <66327737+tvomacka@users.noreply.github.com>
2022-11-25 21:50:15 +0100

    R!! Dialogue Condition Testing

--- Dungeon/GameLogic/Dialogues/DialogueCondition.cs
+++ Dungeon/GameLogic/Dialogues/DialogueCondition.cs
@@ -8,10 +8,10 @@ namespace Dungeon.GameLogic.Dialogues
         public string Test { get; set; }
         public string Target { get; set; }
 
-        public bool IsSatisfied()
+        public bool IsSatisfied(PlayerCharacter playerCharacter)
         {
             if (Subject == "Intelligence" && Test == "GreaterThan")
-                return Game.Instance.Party[0].Intelligence > int.Parse(Target);
+                return playerCharacter.Intelligence > int.Parse(Target);
             else if (Subject == "Item" && Test == "InInventory")
                 return Game.Instance.Party[0].Inventory.Contains(int.Parse(Target));
 
