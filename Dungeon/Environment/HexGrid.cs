f42f01fdbfcd997f10ee6c99496100c79f966371
Tomáš Vomáčka <t.vomacka@seznam.cz>
2022-11-30 22:23:36 +0100

    F!! Added Hexagonal Grid

---
+++ Dungeon/Environment/HexGrid.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon.Environment
+{
+    public class HexGrid
+    {
+        public int Width 
+        { 
+            get;
+            private set;
+        }
+        
+        public int Height 
+        {
+            get;
+            private set;
+        }
+
+        public HexGrid(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public IEnumerable<Point> GetNeighbors(int x, int y)
+        {
+            return new List<Point>() { 
+                new Point(x - 1, y -1),
+                new Point(x - 1, y),
+                new Point(x, y + 1),
+                new Point(x + 1, y),
+                new Point(x + 1, y -1),
+                new Point(x, y -1)
+            };
+        }
+    }
+}
---
+++ DungeonTests/HexGridTests.cs
@@ -0,0 +1,26 @@
+﻿using ApprovalTests.Reporters.Windows;
+using ApprovalTests.Reporters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dungeon.Environment;
+
+namespace DungeonTests
+{
+    [UseReporter(typeof(VisualStudioReporter))]
+    [TestClass]
+    public class HexGridTests
+    {
+        [TestMethod]
+        public void HexagonalGrid_Cell2_2_HasCorrectNeighbors()
+        {
+            var grid = new HexGrid(5, 5);
+            var neighbors = grid.GetNeighbors(2, 2);
+            
+            var n = string.Join(" ", neighbors);
+            Assert.AreEqual("{X=1,Y=1} {X=1,Y=2} {X=2,Y=3} {X=3,Y=2} {X=3,Y=1} {X=2,Y=1}", n);
+        }
+    }
+}
