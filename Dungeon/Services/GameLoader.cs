dc51baccfe7f9be79c40112c78b3e5183396c898
Tomáš Vomáčka <t.vomacka@seznam.cz>
2022-12-13 22:23:23 +0100

    r Namespace Scope Changed

--- Dungeon/Services/GameLoader.cs
+++ Dungeon/Services/GameLoader.cs
@@ -4,35 +4,34 @@ using System.Text.Json.Nodes;
 using System.Text.Json;
 using Dungeon.GameLogic.Equipment;
 
-namespace Dungeon.Services
-{
-    public class GameLoader
-    {
-        public static void Load(Game game, string path) 
-        {
-            string jsonString = File.ReadAllText(path);
-            var json = JsonSerializer.Deserialize<JsonObject>(jsonString);
+namespace Dungeon.Services;
 
-            if (json["Characters"] != null)
-            {
-                game.Characters = JsonSerializer.Deserialize<NonPlayerCharacter[]>(json["Characters"]).ToList<NonPlayerCharacter>();
-            }
-            if (json["Dialogues"] != null)
-            {
-                game.Dialogues = JsonSerializer.Deserialize<Dialogue[]>(json["Dialogues"]).ToList<Dialogue>();
-            }
-            if (json["Party"] != null)
-            {
-                game.Party = JsonSerializer.Deserialize<Party>(json["Party"]);
-            }
-            if (json["Items"] != null)
-            {
-                game.Items = JsonSerializer.Deserialize<Item[]>(json["Items"]).ToList<Item>();
-            }
-            if (json["Quests"] != null)
-            {
-                game.Quests = JsonSerializer.Deserialize<Quest[]>(json["Quests"]).ToList<Quest>();
-            }
+public class GameLoader
+{
+    public static void Load(Game game, string path) 
+    {
+        string jsonString = File.ReadAllText(path);
+        var json = JsonSerializer.Deserialize<JsonObject>(jsonString);
+
+        if (json["Characters"] != null)
+        {
+            game.Characters = JsonSerializer.Deserialize<NonPlayerCharacter[]>(json["Characters"]).ToList<NonPlayerCharacter>();
+        }
+        if (json["Dialogues"] != null)
+        {
+            game.Dialogues = JsonSerializer.Deserialize<Dialogue[]>(json["Dialogues"]).ToList<Dialogue>();
+        }
+        if (json["Party"] != null)
+        {
+            game.Party = JsonSerializer.Deserialize<Party>(json["Party"]);
+        }
+        if (json["Items"] != null)
+        {
+            game.Items = JsonSerializer.Deserialize<Item[]>(json["Items"]).ToList<Item>();
+        }
+        if (json["Quests"] != null)
+        {
+            game.Quests = JsonSerializer.Deserialize<Quest[]>(json["Quests"]).ToList<Quest>();
         }
     }
 }
