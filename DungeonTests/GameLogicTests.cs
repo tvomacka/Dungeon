dcf5fc84d1317f1fa30c0e961558fd6f46cc8730
Tomáš Vomáčka <t.vomacka@seznam.cz>
2022-12-21 21:26:46 +0100

    F!! Game Reset Added

--- DungeonTests/GameLogicTests.cs
+++ DungeonTests/GameLogicTests.cs
@@ -12,8 +12,6 @@ namespace DungeonTests;
 [TestClass]
 public class GameLogicTests
 {
-    private Game game = Game.Instance;
-
     public static void LoadTestGame(string gameName)
     {
         Game.Instance.Load(@"..\..\..\TestResources\Games\" + gameName);
@@ -25,36 +23,47 @@ public class GameLogicTests
     }
 
     [TestMethod]
+    public void GameReset_Clears_AllData()
+    {
+        ResetGame();
+        Assert.AreEqual(0, Game.Instance.Characters.Count());
+        Assert.AreEqual(0, Game.Instance.Quests.Count());
+        Assert.AreEqual(0, Game.Instance.Party.Members.Count());
+        Assert.AreEqual(0, Game.Instance.Items.Count());
+        Assert.AreEqual(Game.ExploreState, Game.Instance.State);
+    }
+
+    [TestMethod]
     public void PartyLocation_CanBeLoadedFromFile()
     {
         LoadTestGame("test.json");
-        Assert.AreEqual("{X=3,Y=5}", game.Party.Location.ToString());
+        Assert.AreEqual("{X=3,Y=5}", Game.Instance.Party.Location.ToString());
     }
 
     [TestMethod]
     public void PartyCharacter_CanBeLoadedFromFile()
     {
         LoadTestGame("party.json");
-        Assert.AreEqual(1, game.Party.Members.Count());
+        Assert.AreEqual(1, Game.Instance.Party.Members.Count());
     }
 
     [TestMethod]
     public void LoadedPlayerCharacter_HasCorrectInt()
     {
         LoadTestGame("party.json");
-        Assert.AreEqual(10, game.Party[0].Intelligence);
+        Assert.AreEqual(10, Game.Instance.Party[0].Intelligence);
     }
 
     [TestMethod]
     public void Party_CanMoveTowardsNpc()
     {
         LoadTestGame("test.json");
-        Assert.AreEqual("{X=3,Y=5}", game.Party.Location.ToString());
+        Assert.AreEqual("{X=3,Y=5}", Game.Instance.Party.Location.ToString());
 
-        var npc = game.GetCharacter("QuestNPC");
-        game.Party.MoveTo(npc.Location.X - 1, npc.Location.Y);
+        var npc = Game.Instance.GetCharacter("QuestNPC");
+        Game.Instance.Party.MoveTo(npc.Location.X - 1, npc.Location.Y);
 
-        Assert.AreEqual("{X=4,Y=5}", game.Party.Location.ToString());
+        Assert.AreEqual("{X=4,Y=5}", Game.Instance.Party.Location.ToString());
     }
 
     [TestMethod]
@@ -62,8 +71,8 @@ public class GameLogicTests
     {
         LoadTestGame("fetchQuest.json");
 
-        Assert.AreEqual(1, game.Items.Count);
-        Assert.AreEqual("Item 0: Quest Item", game.Items[0].ToString());
+        Assert.AreEqual(1, Game.Instance.Items.Count);
+        Assert.AreEqual("Item 0: Quest Item", Game.Instance.Items[0].ToString());
     }
 
     [TestMethod]
@@ -71,8 +80,8 @@ public class GameLogicTests
     {
         LoadTestGame("fetchQuest.json");
 
-        Assert.AreEqual(1, game.Quests.Count);
-        Assert.AreEqual("Quest 0: Fetch Quest Test", game.Quests[0].ToString());
+        Assert.AreEqual(1, Game.Instance.Quests.Count);
+        Assert.AreEqual("Quest 0: Fetch Quest Test", Game.Instance.Quests[0].ToString());
     }
 
     [TestMethod]
@@ -81,7 +90,7 @@ public class GameLogicTests
     {
         LoadTestGame("fetchQuest.json");
 
-        game.PickUpItem(0, 0);
+        Game.Instance.PickUpItem(0, 0);
     }
 
     [TestMethod]
@@ -101,7 +110,7 @@ public class GameLogicTests
     {
         var pc = new PlayerCharacter() { Strength = 10 };
         var hammer = new Weapon() { Id = 1, Name = "Hammer", MinStrength = 12 };
-        game.Items.Add(hammer);
+        Game.Instance.Items.Add(hammer);
         
         pc.Equip(hammer, pc.RightHand);
     }
@@ -111,21 +120,21 @@ public class GameLogicTests
     {
         LoadTestGame("fetchQuest.json");
         
-        game.Party.MoveTo(game.Items[0].Location);
-        game.PickUpItem(0, 0);
+        Game.Instance.Party.MoveTo(Game.Instance.Items[0].Location);
+        Game.Instance.PickUpItem(0, 0);
 
-        Assert.IsTrue(game.Party[0].Inventory.Contains(game.Items[0]));
+        Assert.IsTrue(Game.Instance.Party[0].Inventory.Contains(Game.Instance.Items[0]));
     }
 
     [TestMethod]
     public void FetchQuest_WithoutTheQuestItem_CannotFinish()
     {
         LoadTestGame("fetchQuest.json");
-        var npc = game.GetCharacter("QuestNPC");
-        game.Party.MoveTo(npc.Location.X - 1, npc.Location.Y);
-        game.State = game.DialogueWith(npc);
+        var npc = Game.Instance.GetCharacter("QuestNPC");
+        Game.Instance.Party.MoveTo(npc.Location.X - 1, npc.Location.Y);
+        Game.Instance.State = Game.Instance.DialogueWith(npc);
 
-        Assert.AreEqual(1, (game.State as Dialogue).GetFilteredOptions().Count());
+        Assert.AreEqual(1, (Game.Instance.State as Dialogue).GetFilteredOptions().Count());
     }
 
     [TestMethod]
@@ -133,26 +142,26 @@ public class GameLogicTests
     {
         LoadTestGame("fetchQuest.json");
 
-        Assert.AreEqual(0, game.Party[0].XP);
-        game.Party[0].AddXP(100);
-        Assert.AreEqual(100, game.Party[0].XP);
+        Assert.AreEqual(0, Game.Instance.Party[0].XP);
+        Game.Instance.Party[0].AddXP(100);
+        Assert.AreEqual(100, Game.Instance.Party[0].XP);
     }
 
     [TestMethod]
     public void FetchQuest_Complete_Test()
     {
         LoadTestGame("fetchQuest.json");
-        game.Party.MoveTo(game.Items[0].Location);
-        game.PickUpItem(0, 0);
+        Game.Instance.Party.MoveTo(Game.Instance.Items[0].Location);
+        Game.Instance.PickUpItem(0, 0);
 
-        var npc = game.GetCharacter("QuestNPC");
-        game.Party.MoveTo(npc.Location.X - 1, npc.Location.Y);
-        game.State = game.DialogueWith(npc);
-        game.State = (game.State as Dialogue).ChooseOption(1);
+        var npc = Game.Instance.GetCharacter("QuestNPC");
+        Game.Instance.Party.MoveTo(npc.Location.X - 1, npc.Location.Y);
+        Game.Instance.State = Game.Instance.DialogueWith(npc);
+        Game.Instance.State = (Game.Instance.State as Dialogue).ChooseOption(1);
 
-        Assert.IsFalse(game.Party[0].Inventory.Contains(game.Items[0]));
-        Assert.AreEqual(0, game.Party.ActiveQuests.Count());
-        Assert.AreEqual(100, game.Party[0].XP);
+        Assert.IsFalse(Game.Instance.Party[0].Inventory.Contains(Game.Instance.Items[0]));
+        Assert.AreEqual(0, Game.Instance.Party.ActiveQuests.Count());
+        Assert.AreEqual(100, Game.Instance.Party[0].XP);
     }
 
     [TestMethod]
