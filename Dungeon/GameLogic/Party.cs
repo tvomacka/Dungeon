e722d33232d25686f9b258946502254c263db565
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-01-15 20:51:20 +0100

    r code cleanup

--- Dungeon/GameLogic/Party.cs
+++ Dungeon/GameLogic/Party.cs
@@ -1,35 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Dungeon.GameLogic;
 
-namespace Dungeon.GameLogic
+public class Party
 {
-    public class Party
+    public Point Location { get; set; }
+    public List<PlayerCharacter> Members { get; set; }
+    public PlayerCharacter this[int index]
     {
-        public Point Location { get; set; }
-        public List<PlayerCharacter> Members { get; set; }
-        public PlayerCharacter this[int index]
-        {
-            get { return Members[index]; }
-        }
-        public List<int> AssignedQuests { get; set; }
+        get { return Members[index]; }
+    }
+    public List<int> AssignedQuests { get; set; }
 
-        public Party()
-        {
-            Members = new List<PlayerCharacter>();
-            AssignedQuests = new List<int>();
-        }
+    public Party()
+    {
+        Members = new List<PlayerCharacter>();
+        AssignedQuests = new List<int>();
+    }
 
-        public void MoveTo(int x, int y)
-        {
-            Location = new Point(x, y);
-        }
+    public void MoveTo(int x, int y)
+    {
+        Location = new Point(x, y);
+    }
 
-        public void MoveTo(Point location)
-        {
-            MoveTo(location.X, location.Y);
-        }
+    public void MoveTo(Point location)
+    {
+        MoveTo(location.X, location.Y);
     }
 }
